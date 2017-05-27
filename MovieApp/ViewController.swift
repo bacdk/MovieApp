f9fd232f683de8d876696346ac9582dc6bd4e72c
Khacbac <daukhacbac@gmail.com>
2017-05-27 16:19:54 +0700

    no message

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Cntt20 on 5/27/17.
+//  Copyright © 2017 Dau Khac Bac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
