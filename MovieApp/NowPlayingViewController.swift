a8f43cc6fcfa4f1c62cf224ba7ea76b80a7d8668
miceli <tthanhkhoa@gmail.com>
2017-06-02 23:29:47 +0700

    no message

--- MovieApp/NowPlayingViewController.swift
+++ MovieApp/NowPlayingViewController.swift
@@ -1,14 +1,16 @@
 //
-//  NowPlayingTVController.swift
-//  MovieApp
+//  NowPlayingViewController.swift
+//  MovieApp-master
 //
-//  Created by Cntt22 on 5/27/17.
+//  Created by miceli on 6/2/17.
 //  Copyright © 2017 Dau Khac Bac. All rights reserved.
 //
 
 import UIKit
 
-class NowPlayingTVController: UITableViewController {
+class NowPlayingViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
 
     var movies = [Movie]()
     
@@ -20,6 +22,8 @@ class NowPlayingTVController: UITableViewController {
         for movie in jsonListMovie {
             movies.append(Movie(json: movie as! [String:Any]))
         }
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
     }
     
     override func didReceiveMemoryWarning() {
@@ -29,13 +33,13 @@ class NowPlayingTVController: UITableViewController {
     
     // MARK: - Table view data source
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return movies.count
     }
     
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "NowTVCell", for: indexPath) as! NowPlayingTVCell
         //        var queue = OperationQueue()
         //        let operation1 = BlockOperation(block: {
@@ -70,55 +74,11 @@ class NowPlayingTVController: UITableViewController {
     // MARK: - Segues
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         //if segue.identifier == "showDetail" {
-            
-          //  }
-        }
+        
+        //  }
     }
+}
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     /*
     // MARK: - Navigation
@@ -129,3 +89,4 @@ class NowPlayingTVController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
+
