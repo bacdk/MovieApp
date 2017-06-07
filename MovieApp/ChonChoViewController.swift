3301bd130008e632c443ce4db6fd201e8eacc5ce
miceli <tthanhkhoa@gmail.com>
2017-06-08 00:12:14 +0700

    add book ticket screen

---
+++ MovieApp/ChonChoViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ChonChoViewController.swift
+//  MovieApp-master
+//
+//  Created by Cntt23 on 6/7/17.
+//  Copyright © 2017 Dau Khac Bac. All rights reserved.
+//
+
+import UIKit
+
+class ChonChoViewController: UIViewController {
+    
+    //Outlet
+    //Cho Ngoi
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //Thanh toan
+    @IBAction func thanhToan(_ sender: Any) {
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
