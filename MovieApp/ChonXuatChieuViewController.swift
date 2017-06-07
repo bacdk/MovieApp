5106d4d1d1e43cf013b58db61cfc2abcd1a640f5
Cntt23 <Admin23@Cntt23s-iMac.local>
2017-06-07 16:33:58 +0700

    Change

---
+++ MovieApp/ChonXuatChieuViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ChonXuatChieuViewController.swift
+//  MovieApp-master
+//
+//  Created by Cntt23 on 6/7/17.
+//  Copyright © 2017 Dau Khac Bac. All rights reserved.
+//
+
+import UIKit
+
+class ChonXuatChieuViewController: UIViewController {
+    
+    //Outlet
+    //Ngay
+    @IBOutlet weak var homNay: UIButton!
+    @IBOutlet weak var ngayMai: UIButton!
+    //Gio
+    @IBOutlet weak var gio9AM: UIButton!
+    @IBOutlet weak var gio1110AM: UIButton!
+    @IBOutlet weak var gio12PM: UIButton!
+    @IBOutlet weak var gio150PM: UIButton!
+    @IBOutlet weak var gio430PM: UIButton!
+    @IBOutlet weak var gio6PM: UIButton!
+    @IBOutlet weak var gio710PM: UIButton!
+    @IBOutlet weak var gio945PM: UIButton!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    //Hom nay
+    @IBAction func isHomNay(_ sender: Any) {
+        
+    }
+    
+    //Ngay mai
+    @IBAction func isNgayMai(_ sender: Any) {
+        
+    }
+    
+    //Chon gio
+    @IBAction func chonGio(_ sender: Any) {
+        
+    }
+    
+    //Chon cho
+    @IBAction func chonCho(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+
+}
